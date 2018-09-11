d352ef917327bd9d202a36c0887b8050484f528e
jedelson <edelson.m.jason@gmail.com>
2018-09-11 12:49:38 -0700

    settings.py added

--- testgui/settings.py
+++ testgui/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
+import django_heroku
+from whitenoise import WhiteNoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,8 +28,7 @@ SECRET_KEY = 'hu2w!!#k95ip2e%*))dh)k%$k*#j3$!x1o!p1rf!r_lb!0t^3b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+#DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 
 # Application definition
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainApp',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'debug': DEBUG,
         },
     },
 ]
@@ -104,17 +109,35 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'America/Los_Angeles'
 USE_I18N = True
-
 USE_L10N = True
+USE_TZ = False
 
-USE_TZ = True
+# Change 'default' database configuration with $DATABASE_URL.
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+# Django security check
+X_FRAME_OPTIONS = 'DENY'
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER= True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+LOGIN_URL= '/login'
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Activate Django-Heroku.
+django_heroku.settings(locals())
---
+++ testgui/static/js/main.js
@@ -0,0 +1,182 @@
+$("#home").css("min-height", screen.height-270)
+$(document).ready(function() {
+
+  var oTable = $('#myTable').DataTable(
+    {"dom": 'lrtip'}
+  );   //pay attention to capital D, which is mandatory to retrieve "api" datatables' object, as @Lionel said
+  $('#myInputTextField').keyup(function(){
+    column = $('#inputGroupSelect01').val()
+    if(column=='0'){
+      oTable.search($(this).val()).draw() ; //all
+    }
+    else if(column=='1'){
+      oTable.columns(0).search($(this).val()).draw() ;//handle col 0
+    }
+    else if(column=='2'){
+      oTable.columns(1).search($(this).val()).draw() ;//name col 1
+    }
+    else if(column=='3'){
+      oTable.columns(2).search($(this).val()).draw() ;//email col2
+    }
+    else if(column=='4'){
+      oTable.columns(3).search($(this).val()).draw() ;//twitch
+    }
+    else if(column=='5'){
+      oTable.columns(4).search($(this).val()).draw() ;//twitter
+    }
+    else if(column=='6'){
+      oTable.columns(5).search($(this).val()).draw() ;//country
+    }
+    else if(column=='7'){
+      oTable.columns(6).search($(this).val()).draw() ;//shirt
+    }
+    else if(column=='8'){
+      oTable.columns(8).search($(this).val()).draw() ;//update
+    }
+    else if(column=='9'){
+      oTable.columns(9).search($(this).val()).draw() ;//mailing_address
+    }
+    else if(column=='10'){
+      oTable.columns(10).search($(this).val()).draw() ;//phone
+    }
+    else if(column=='11'){
+      oTable.columns(11).search($(this).val()).draw() ;//youtube
+    }
+    else if(column=='12'){
+      oTable.columns(12).search($(this).val()).draw() ;//mixer
+    }
+    else if(column=='13'){
+      oTable.columns(13).search($(this).val()).draw() ;//tags
+    }
+    else if(column=='14'){
+      oTable.columns(14).search($(this).val()).draw() ;//events
+    }
+    else if(column=='15'){
+      oTable.columns(15).search($(this).val()).draw() ;//lists
+    }
+
+  })
+  $('#inputGroupSelect01').change(function(){
+    oTable.search('').draw() ;
+    column = $('#inputGroupSelect01').val()
+    if(column=='0'){
+      oTable.search($('#myInputTextField').val()).draw() ; //all
+    }
+    else if(column=='1'){
+      oTable.columns(0).search($('#myInputTextField').val()).draw() ;//handle col 0
+    }
+    else if(column=='2'){
+      oTable.columns(1).search($('#myInputTextField').val()).draw() ;//name col 1
+    }
+    else if(column=='3'){
+      oTable.columns(2).search($('#myInputTextField').val()).draw() ;//email col2
+    }
+    else if(column=='4'){
+      oTable.columns(3).search($('#myInputTextField').val()).draw() ;//twitch
+    }
+    else if(column=='5'){
+      oTable.columns(4).search($('#myInputTextField').val()).draw() ;//twitter
+    }
+    else if(column=='6'){
+      oTable.columns(5).search($('#myInputTextField').val()).draw() ;//country
+    }
+    else if(column=='7'){
+      oTable.columns(6).search($('#myInputTextField').val()).draw() ;//shirt
+    }
+    else if(column=='8'){
+      oTable.columns(8).search($('#myInputTextField').val()).draw() ;//update
+    }
+    else if(column=='9'){
+      oTable.columns(9).search($('#myInputTextField').val()).draw() ;//mailing_address
+    }
+    else if(column=='10'){
+      oTable.columns(10).search($('#myInputTextField').val()).draw() ;//phone
+    }
+    else if(column=='11'){
+      oTable.columns(11).search($('#myInputTextField').val()).draw() ;//youtube
+    }
+    else if(column=='12'){
+      oTable.columns(12).search($('#myInputTextField').val()).draw() ;//mixer
+    }
+    else if(column=='13'){
+      oTable.columns(13).search($('#myInputTextField').val()).draw() ;//tags
+    }
+    else if(column=='14'){
+      oTable.columns(14).search($('#myInputTextField').val()).draw() ;//events
+    }
+    else if(column=='15'){
+      oTable.columns(15).search($('#myInputTextField').val()).draw() ;//lists
+    }
+  })
+
+})
+function tags_filter(){
+  var item;
+  var input = document.getElementById('search-tags');
+  var filter = input.value.toUpperCase();
+  var list_items = document.getElementById("id_tags").childNodes
+  for(var i=1; i<list_items.length; i+=2){
+    item = list_items[i];
+    if(item.textContent.toUpperCase().indexOf(filter) == -1){
+      list_items[i].style.display = "none";
+    }
+    else{
+      list_items[i].style.display = '';
+    }
+  }
+}
+function events_filter(){
+  var item;
+  var input = document.getElementById('search-events');
+  var filter = input.value.toUpperCase();
+  var list_items = document.getElementById("id_events").childNodes
+  for(var i=1; i<list_items.length; i+=2){
+    item = list_items[i];
+    if(item.textContent.toUpperCase().indexOf(filter) == -1){
+      list_items[i].style.display = "none";
+    }
+    else{
+      list_items[i].style.display = '';
+    }
+  }
+}
+function lists_filter(){
+  var item;
+  var input = document.getElementById('search-lists');
+  var filter = input.value.toUpperCase();
+  var list_items = document.getElementById("id_list_influencers").childNodes
+  for(var i=1; i<list_items.length; i+=2){
+    item = list_items[i];
+    if(item.textContent.toUpperCase().indexOf(filter) == -1){
+      list_items[i].style.display = "none";
+    }
+    else{
+      list_items[i].style.display = '';
+    }
+  }
+}
+function helper_button_handler(btn_value){
+  textbox = $("#id_seperate_fields_with_commas").val()
+  textbox = textbox.trim()
+  if (textbox.length == 0){
+    textbox = btn_value;
+  }
+  else if (textbox.endsWith(",")){
+    textbox = textbox + ' ' + btn_value;
+  }
+  else{
+    textbox = textbox + ', ' + btn_value;
+  }
+  $("#id_seperate_fields_with_commas").val(textbox)
+
+
+}
+if ( $( "#id_tags" ).length ) {
+    $( "#id_tags" ).before("<input type='text' id='search-tags' onkeyup='tags_filter()' placeholder='Search for Tags'></input>");
+}
+if ( $( "#id_events" ).length ) {
+    $( "#id_events" ).before("<input type='text' id='search-events' onkeyup='events_filter()' placeholder='Search for Events'></input>");
+}
+if ( $( "#id_list_influencers" ).length ) {
+    $( "#id_list_influencers" ).before("<input type='text' class='mb-1' id='search-lists' onkeyup='lists_filter()' placeholder='Search for Lists'></input>");
+}
