28a7e5b58fee45188abc85bafd1ac508f55f10c8
jedelson <edelson.m.jason@gmail.com>
2018-09-11 15:48:06 -0700

    everything working so far
    up and running on heroku

--- mainApp/views.py
+++ mainApp/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
+
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
