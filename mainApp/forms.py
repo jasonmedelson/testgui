4d7fad69c4aa488b1592662472a0958f1c52cc6b
jedelson <edelson.m.jason@gmail.com>
2018-09-11 12:53:26 -0700

    models, forms, urls

---
+++ mainApp/forms.py
@@ -0,0 +1,73 @@
+from django import forms
+from .models import Influencer, Tags, Events, List
+
+class InfluencerCreateForm(forms.ModelForm):
+
+    class Meta:
+        model = Influencer
+        fields = [
+        'influencer_handle',
+        'legal_name',
+        'email',
+        'phone',
+        'twitter',
+        'youtube',
+        'twitch',
+        'mixer',
+        'shirt',
+        'country',
+        'mailing_address',
+        'notes',
+        'tags',
+        'events',
+        ]
+        widgets = {
+            # 'tags': FilteredSelectMultiple("Tags",is_stacked=False,choices=Tags.objects.all()),
+            'tags': forms.CheckboxSelectMultiple(),
+            'events': forms.CheckboxSelectMultiple(),
+        }
+    def __init__(self, user, *args, **kwargs):
+        # self.user = kwargs.pop('user', None)
+        # del kwargs['user']
+
+        super(InfluencerCreateForm, self).__init__(*args, **kwargs)
+        self.fields['tags'].queryset = Tags.objects.filter(tag_user=user)
+        self.fields['events'].queryset = Events.objects.filter(event_user=user)
+
+class InfluencerCSVForm(forms.Form):
+    seperate_fields_with_commas = forms.CharField(max_length=200)
+    paste_CSV = forms.CharField(widget=forms.Textarea)
+
+class TagFormCSV(forms.Form):
+    seperate_tags_with_commas = forms.CharField(widget=forms.Textarea)
+
+class EventFormCSV(forms.Form):
+    seperate_events_with_commas = forms.CharField(widget=forms.Textarea)
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tags
+        fields = [
+        'tag_name'
+        ]
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Events
+        fields = [
+        'event_name'
+        ]
+
+class ListCreateForm(forms.ModelForm):
+    class Meta:
+        model = List
+        fields = [
+        'list_name',
+        'list_influencers',
+        ]
+        widgets = {
+        'list_influencers': forms.CheckboxSelectMultiple(),
+        }
+    def __init__(self, user, *args, **kwargs):
+        super(ListCreateForm, self).__init__(*args, **kwargs)
+        # self.fields['list_influencers'].queryset = Influencer.objects.filter(user=user)
--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,114 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.urls import reverse
+import uuid
 # Create your models here.
+
+
+class Tags(models.Model):
+    tag_name = models.CharField(max_length=40)
+    tag_user = models.ForeignKey(User,on_delete=models.CASCADE,)
+    def __str__(self):
+        return self.tag_name
+    def get_absolute_url(self):
+        return reverse('tag-update', kwargs={'pk': self.id})
+    class Meta:
+        ordering = ('tag_name',)
+
+class Events(models.Model):
+    event_name = models.CharField(max_length=40)
+    event_user = models.ForeignKey(User,on_delete=models.CASCADE,)
+    def __str__(self):
+        return self.event_name
+    def get_absolute_url(self):
+        return reverse('event-update', kwargs={'pk': self.id})
+    class Meta:
+        ordering = ('event_name',)
+
+class Influencer(models.Model):
+    XSMALL = 'XS'
+    SMALL = 'SM'
+    MEDIUM = 'MD'
+    LARGE = 'LG'
+    XLARGE = 'XL'
+    XXLARGE = '2X'
+    XXXLARGE = '3X'
+    XXXXLARGE = '4X'
+    XXXXXLARGE = '5X'
+    SHIRT_SIZE_CHOICES = (
+        (XSMALL, 'X Small'),
+        (SMALL, 'Small'),
+        (MEDIUM, 'Medium'),
+        (LARGE, 'Large'),
+        (XLARGE, 'X Large'),
+        (XXLARGE, '2X Large'),
+        (XXXLARGE, '3X Large'),
+        (XXXXLARGE, '4X Large'),
+        (XXXXXLARGE, '5X Large'),
+    )
+    US = 'USA'
+    CANADA = 'CAN'
+    SAMERICA = 'SAM'
+    UK = 'UNK'
+    FRANCE= 'FRA'
+    ASIA= 'ASN'
+    OTHEREURO = 'OEU'
+    OTHER = 'OTH'
+    COUNTRY_CHOICES = (
+        (US, 'United States'),
+        (CANADA, 'Canada'),
+        (UK, 'United Kingdom'),
+        (SAMERICA, 'South America'),
+        (FRANCE, 'France'),
+        (ASIA, 'Asia'),
+        (OTHEREURO, 'Europe'),
+        (OTHER, 'Other'),
+    )
+    user = models.ForeignKey(User,on_delete=models.CASCADE,)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    influencer_handle = models.CharField(max_length=50)
+    legal_name = models.CharField(max_length=50,blank = True)
+    email = models.EmailField(blank = True)
+    mailing_address = models.TextField(blank = True)
+    phone = models.CharField(max_length=20,blank = True)
+    shirt = models.CharField(
+        max_length=2,
+        choices = SHIRT_SIZE_CHOICES,
+        default = XLARGE,
+        blank = True,
+    )
+    country = models.CharField(
+        max_length=3,
+        choices = COUNTRY_CHOICES,
+        blank = True,
+        default=US
+        )
+    twitter = models.CharField(max_length=50, blank = True)
+    youtube = models.CharField(max_length=50, blank = True)
+    twitch = models.CharField(max_length=50, blank = True)
+    mixer = models.CharField(max_length=50, blank = True)
+    notes = models.TextField(blank = True,)
+    tags = models.ManyToManyField(Tags,blank=True)
+    events = models.ManyToManyField(Events,blank=True)
+
+    def __str__(self):
+        return self.influencer_handle
+
+    def get_absolute_url(self):
+        return reverse('influencer-update', kwargs={'pk': self.id})
+    class Meta:
+        ordering = ('influencer_handle',)
+
+class List(models.Model):
+    list_user = models.ForeignKey(User,on_delete=models.CASCADE,)
+    list_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    list_name = models.CharField(max_length=50)
+    list_influencers = models.ManyToManyField(Influencer,blank=True)
+    def __str__(self):
+        return self.list_name
+    def get_absolute_url(self):
+        return reverse('list-update', kwargs={'pk': self.list_id})
+    class Meta:
+        ordering = ('list_name',)
